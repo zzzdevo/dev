3685c0ab1f6ccadff4bdfd742c43ea0fe3bfbd8d
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-28 00:45:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ r = redis.Redis(
     decode_responses=True
     )
 
-sudo_id = 5964012315
-bot_user = "jvfhhcfhhbot"
-api_id = 10823881
-api_hash = "339886e2109eb67203ce12022b32e035"
-session = "BABJpCyfNw6x-3aCTepEtHHLiH8frdPeLwwL_pMBer_ITlUYv-98QafvSHsOZTQmQWNuFGHBq8YzoISSQljsOw_-XmotDYn8dn3C8jVfyhAR8w47Bza7maVEPHNQlOE4dhwSe2It1yiK8ryI-7s54SQ0318e5o8oTS7QivJVBawdzLD-6ChYV8JYpGd1zO_5d6S0S9k0b4sq5C3smow6KETPrXZ19e9xwjJkLhRSKPKo5RBLnoC331t6K3cjN4RcoG8pQiUlYgLW9YlF3NkcHK0CtDyaNrVPmeoEHSS5j0wVjprzgajrbVmTZpi4bkja8lMwVkqubUY2mviyK6V3ygoHAAAAAWN7mxsA"
-token = "6174014309:AAEPXnzyWgOO0ESVUUIqKDlpNOZwCeRHVD0"
-sudo_command = [1804133252, 5912085775]
-pm = "5964012315"
-mention = "5964012315"
+sudo_id = 833360381
+bot_user = "IQDVBOT"
+api_id = 12962251
+api_hash = "b51499523800add51e4530c6f552dbc8"
+session = "AgAhIHQAbL3A-bLILW5GWxODfKaejkGhpwTdJwT4-ILVxYfhMilcPV_W4mwKbfW_Mjc9yjEfmGV3ikctmQl1sGUWMKCTQx6WUCCYmRbrah8kkZVbAvq3MGIH_75D06_zt23t2baRmqVp-qkLgQeKOxKw6I6MvXuFm2nDrch6KOc3DpgGGSHrtP59Uzywl_93y2K0MBxRjANZQFyPKlKwxmKczjHafW8oum9K49B6KJ1Gbk_8XAu5Ra5T-TMO1yY1BI9wqW62z_CboFZ1mbYx95VGTZ3RpaLe7KFCruWwipWykK4w4CGykvfgaLXqzaBpjUHHd7c-uz_mlqdShRuYQtFE41-osAAAAAAxrBH9AA"
+token = "6475201483:AAF5RMcasWoHV1K_jYsdYiP6CG7jkGDNNGQ"
+sudo_command = [833360381, 1818734394]
+pm = "833360381"
+mention = "833360381"
 plugins = dict(root="plugins")
-app = Client("user:jvfhhcfhhbot",api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
-bot = Client("jvfhhcfhhbot",api_id=api_id , api_hash=api_hash ,bot_token=token,plugins=dict(root="plug_bot"))
+app = Client("user:IQDVBOT",api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
+bot = Client("IQDVBOT",api_id=api_id , api_hash=api_hash ,bot_token=token,plugins=dict(root="plug_bot"))
